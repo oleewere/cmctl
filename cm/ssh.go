511c9e283a3c68b530bbd45baec22bc35085bd59
Oliver Szabo <oleewere@gmail.com>
2019-05-07 17:26:37 +0200

    Move ssh util to ssh file

--- cm/ssh.go
+++ cm/ssh.go
@@ -3,6 +3,7 @@ package cm
 import (
 	"fmt"
 	"os"
+	"os/exec"
 	"strconv"
 	"strings"
 	"sync"
@@ -133,6 +134,23 @@ func (c CMServer) RunRemoteHostCommand(command string, filteredHosts map[string]
 	}
 	wg.Wait()
 	return response
+}
+
+// DownloadViaScp downloads file from remote to local
+func DownloadViaScp(sshConfig *easyssh.MakeConfig, source string, dest string, skipJump bool) error {
+	userAndRemote := fmt.Sprintf("%v@%v", sshConfig.User, sshConfig.Server)
+	var args []string
+	if len(sshConfig.Proxy.Server) > 0 && !skipJump {
+		args = []string{"-o", fmt.Sprintf("ProxyJump=%v", sshConfig.Proxy.Server), "-o", "StrictHostKeyChecking=no", "-q", "-P", sshConfig.Port, "-i", sshConfig.KeyPath, userAndRemote + ":" + source, dest}
+	} else {
+		args = []string{"-o", "StrictHostKeyChecking=no", "-q", "-P", sshConfig.Port, "-i", sshConfig.KeyPath, userAndRemote + ":" + source, dest}
+	}
+	cmd := exec.Command("scp", args...)
+	if err := cmd.Run(); err != nil {
+		return err
+	}
+	fmt.Println(fmt.Sprintf("Copy %v (host: %v) to location: %v", source, sshConfig.Server, dest))
+	return nil
 }
 
 func createSshConfig(connectionProfile ConnectionProfile, host string, gatewayHost string) *easyssh.MakeConfig {
